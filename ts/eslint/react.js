bec24636bdb095a61907fac813e202558dcc5903
Kryštof Řezáč <krystofrezac@gmail.com>
2020-10-22 17:12:11 +0200

    Add dependecies

--- ts/eslint/react.js
+++ ts/eslint/react.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  plugins: ['react', '@typescript-eslint'],
   settings: {
     'import/resolver': {
       typescript: {},
